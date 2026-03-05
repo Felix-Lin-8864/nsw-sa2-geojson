2a2cbd9ea494c2d10a702bd210c2f6db0496ed72
Felix Lin <felixlin77@gmail.com>
2026-03-05 22:54:38 +1100

    Configure gh-pages deployment

---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite';
+
+// IMPORTANT: if your GitHub repo name is not "nsw-sa2-geojson",
+// change the base path below to match the repo name: `/<your-repo-name>/`
+export default defineConfig({
+  base: '/nsw-sa2-geojson/',
+});
